88addcf3f98e2e217157a6779b05ac094e7d02cc
Sasha Okoronko <hvorost22@mail.ru>
2022-02-23 02:33:09 +0300

    PR-2: - fix small bugs

--- delivery/http/products.js
+++ delivery/http/products.js
@@ -3,7 +3,7 @@ const router = new Router();
 const ProductController = require("../../controllers/ProductController");
 
 router.post("/products", ProductController.create);
-router.post("/products", ProductController.update);
-router.post("/products/:uuid", ProductController.delete);
+router.put("/products", ProductController.update);
+router.delete("/products/:uuid", ProductController.delete);
 
 module.exports = router;
