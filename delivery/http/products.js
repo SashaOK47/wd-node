5807934182a8109e02fd4356c1ce591e7f1d8390
Sasha Okoronko <hvorost22@mail.ru>
2022-02-23 02:23:39 +0300

    PR-2: - created db; installed sequelize; wrote an api request to create a product in the database

--- delivery/http/products.js
+++ delivery/http/products.js
@@ -1,53 +1,9 @@
 const Router = require("express");
 const router = new Router();
+const ProductController = require("../../controllers/ProductController");
 
-const response = {
-  error: null,
-  products: [],
-};
-
-// insert
-router.post("/products", async (req, res) => {
-  let product = {};
-  const { title, photo, price, manufacturer } = req.body;
-
-  if (!title.trim() || !manufacturer.trim) {
-    response.error = "All fields are required";
-  } else if (typeof price !== "number") {
-    response.error = "The price field must be a number";
-  } else if (price < 0) {
-    response.error = "The price field cannot be negative";
-  } else {
-    product = {
-      id: response.products.length + 1,
-      title,
-      photo: null,
-      price,
-      manufacturer,
-    };
-    response.products.push(product);
-  }
-
-  if (response.error) {
-    res.status(500).json(response.error || new Error("UC undefined error"));
-    return;
-  }
-  res.status(200).json(response.products);
-});
-
-// select *
-router.get("/products", async (req, res) => {
-  if (response.error) {
-    res.status(500).json(response.error || new Error("UC undefined error"));
-    return;
-  }
-  res.status(200).json(response.products);
-});
-
-// update
-router.put("/products", async (req, res) => {});
-
-// delete
-router.delete("/products/:id", async (req, res) => {});
+router.post("/products", ProductController.create);
+router.post("/products", ProductController.update);
+router.post("/products/:uuid", ProductController.delete);
 
 module.exports = router;
---
+++ migrations/20220222213644-create-product.js
@@ -0,0 +1,44 @@
+"use strict";
+module.exports = {
+  async up(queryInterface, DataTypes) {
+    await queryInterface.createTable("products", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      uuid: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      photo: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      price: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      manufacturer: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    });
+  },
+  async down(queryInterface, DataTypes) {
+    await queryInterface.dropTable("products");
+  },
+};
