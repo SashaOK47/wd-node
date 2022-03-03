19d36ecb7530da4e743cbdc41209840047144a99
Sasha Okoronko <hvorost22@mail.ru>
2022-03-03 03:54:42 +0300

    PR-5: - made viewing and changing the status of the order

--- delivery/http/orders.js
+++ delivery/http/orders.js
@@ -5,5 +5,7 @@ const OrderController = require("../../controllers/OrderController");
 router.post("/orders", OrderController.create);
 router.get("/orders", OrderController.getAll);
 router.get("/orders/:uuid", OrderController.getOne);
+router.get("/status/:uuid", OrderController.getStatus);
+router.put("/status/:uuid", OrderController.changeStatus);
 
 module.exports = router;
